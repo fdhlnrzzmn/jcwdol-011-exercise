b8f7bbf97a5b71f5444ac79d0d2d7ae34db5ff1a
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2023-08-03 13:22:42 +0800

    task 6

--- exercise-6.js
+++ exercise-6.js
@@ -1,71 +1,71 @@
 // Create a function to calculate array of student data
-// {
-// const calculateStudentData = (studentsArray) => {
+{
+const calculateStudentData = (studentsArray) => {
 
-//     let result = {
-//         score: {
-//             highest: studentsArray[0].score,
-//             lowest:studentsArray[0].score,
-//             average:0,
-//         },
-//         age:{
-//             highest: (new Date() - studentsArray[0].age) / 31536000000,
-//             lowest: (new Date() - studentsArray[0].age) / 31536000000,
-//             average:0,
-//         },
-//     };
+    let result = {
+        score: {
+            highest: studentsArray[0].score,
+            lowest:studentsArray[0].score,
+            average:0,
+        },
+        age:{
+            highest: (new Date() - studentsArray[0].age) / 31536000000,
+            lowest: (new Date() - studentsArray[0].age) / 31536000000,
+            average:0,
+        },
+    };
 
-//     let totalScore = 0;
-//     let totalAge = 0;
+    let totalScore = 0;
+    let totalAge = 0;
 
-//     for(let i = 0; i < studentsArray.length; i++){
-//         let years = (new Date() - studentsArray[i].age) / 31536000000;
-//         if (studentsArray[i].score > result.score.highest) {
-//             result.score.highest = studentsArray[i].score;
-//         }
-//         if (studentsArray[i].score < result.score.lowest) {
-//             result.score.lowest = studentsArray[i].score
-//         } 
-//         if (years > result.age.highest) {
-//             result.age.highest = studentsArray[i].age;
-//         }
-//         if (years < result.age.highest){
-//             result.age.lowest = studentsArray[i].age;
-//         }
-//         totalScore += studentsArray[i].score;
-//         totalAge += years;
-//     }
+    for(let i = 0; i < studentsArray.length; i++){
+        let years = (new Date() - studentsArray[i].age) / 31536000000;
+        if (studentsArray[i].score > result.score.highest) {
+            result.score.highest = studentsArray[i].score;
+        }
+        if (studentsArray[i].score < result.score.lowest) {
+            result.score.lowest = studentsArray[i].score
+        } 
+        if (years > result.age.highest) {
+            result.age.highest = studentsArray[i].age;
+        }
+        if (years < result.age.highest){
+            result.age.lowest = studentsArray[i].age;
+        }
+        totalScore += studentsArray[i].score;
+        totalAge += years;
+    }
     
-//     result.score.average = totalScore / studentsArray.length;
-//     result.age.average = totalAge / studentsArray.length;
+    result.score.average = totalScore / studentsArray.length;
+    result.age.average = totalAge / studentsArray.length;
 
-//     return result;
+    return result;
 
-// }
+}
 
-// const studentsArray = [
-//     {
-//         name: "Luffy",
-//         email:"luffy@onepiece.com",
-//         age: new Date("1999-1-1"),
-//         score:100,
-//     },
-//     {
-//         name: "Ace",
-//         email:"ace@onepiece.com",
-//         age: new Date("1997-1-1"),
-//         score:90,
-//     },
-//     {
-//         name: "Sabo",
-//         email:"sabo@onepiece.com",
-//         age: new Date("1998-1-1"),
-//         score:80,
-//     }
-// ];
+const studentsArray = [
+    {
+        name: "Luffy",
+        email:"luffy@onepiece.com",
+        age: new Date("1999-1-1"),
+        score:100,
+    },
+    {
+        name: "Ace",
+        email:"ace@onepiece.com",
+        age: new Date("1997-1-1"),
+        score:90,
+    },
+    {
+        name: "Sabo",
+        email:"sabo@onepiece.com",
+        age: new Date("1998-1-1"),
+        score:80,
+    }
+];
 
-// console.info(calculateStudentData(studentsArray))
-// }
+console.info(calculateStudentData(studentsArray))
+}
 
 
 // Create a program to create transaction
