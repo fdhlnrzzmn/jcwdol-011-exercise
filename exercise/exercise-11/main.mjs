a69d2fdcdb71e4b68090efdb9fce80ca00809bc9
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2023-08-16 22:50:34 +0800

    Delete exercise directory

--- exercise/exercise-11/main.mjs
+++
@@ -1,9 +0,0 @@
-import {Queue} from "./queue.mjs"
-// const Queue = require("./queue.mjs");
-const queue = new Queue();
-
-queue.addOrder("Bakso");
-queue.addOrder("Ayam Bakar");
-queue.addOrder("Bebek Goreng");
-console.info(queue.showOrder());
-queue.processOrder();
